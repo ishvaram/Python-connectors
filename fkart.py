ccc155745d6f048aeda2c472878b401caff38b29
ishvaram <jehovaram@gmail.com>
2015-12-23 22:04:06 +0530

    check flipkart data on the field

--- fkart.py
+++ fkart.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import sys
+import os
 import urllib2
 import itertools
 import requests
 import MySQLdb
 import json
+import simplejson as json
 
 conn=MySQLdb.connect (host ="localhost",user="root",passwd="root",db="flipkart")    
 product = raw_input("Enter product name: ")
@@ -21,34 +23,32 @@ for data in link:
     #links = url.find('a')['href']
     #print ("http://www.flipkart.com"+links)
     # print (" ")
-    #for data in links:             
+    #for data in links:      	
         raw = opener.open(data).read()
         soup = BeautifulSoup(raw)
         title = soup.findAll('h1','title')
         for eachitem in title:
-                name1 = eachitem.text.strip()
+                name1 = eachitem.text.strip().encode('utf-8')
                 name1 = "'"+name1+"'"                            
 
         price = soup.findAll('span','selling-price omniture-field')
         for eachprice in price:
-                price1 = eachprice.text.strip() 
+                price1 = eachprice.text.strip().encode('utf-8')
                 price1 = "'"+price1+"'"                               
-
-        keyFeatures = soup.findAll('div',class_='keyFeatures specSection')
-        for keys in keyFeatures:
-                keyFeatures1 = keys.text.strip()
-                keyFeatures1 = "'"+keyFeatures1+"'"
+        keyfeat12 = {}
+        keyfeatures = soup.findAll('div','keyFeatures specSection')        
+        for keys in keyfeatures:
+        	keyfeat = keys.text.strip().encode('utf-8')
+        	keyfeat12 = "'"+keyfeat+"'"				
 
         main_dict = {}
         soup = BeautifulSoup(requests.get(data).text)
         Specification = soup.findAll('table','specTable')        
         #Specification.findAll('tr',recursive=False)
-        [main_dict.update(x) for x in [dict([tuple([y.renderContents().strip() for y in x.findAll('td')]) for x in Spec.findAll('tr') if len(x.findAll('td'))==2]) for Spec in Specification]]
-        
-            
+        [main_dict.update(x) for x in [dict([tuple([y.renderContents().strip() for y in x.findAll('td')]) for x in Spec.findAll('tr') if len(x.findAll('td'))==2]) for Spec in Specification]]                
         cursore=conn.cursor()       
-        x="insert into fkartmobile (name,price,keyfeatures,specs) values (%s , %s , %s , %s)"%(name1,price1,keyFeatures1,main_dict)        
-        cursore.execute(x)
+        datum="insert into fkartmobile (name,price,keyfeatures,specs,url) values (%s , %s , %s , %s, %s)"                                                        
+        cursore.execute(datum,(name1,price1,json.dumps(keyfeat12),json.dumps(main_dict),data))
         conn.commit()                
 
 
